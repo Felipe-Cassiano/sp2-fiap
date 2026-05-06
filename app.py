b09d77353646bb6334296e7102bd67bd53ccf7fe
Felipe Cassiano <felipe3.cassiano@gmail.com>
2026-05-06 16:59:30 -0300

    feat: IA reconhece imagens
    IA agora recebe e analia imagens dropadas na drop-zone do site, retornando uma mensagem no console de confirmação e uma analise simples da imagem.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+#TODOS OS COMENTÁRIOS FORAM FEITOS USANDO A EXTENSÃO "Better Comments" DO VSCODE, 
+#PARA EXPLICAR CADA PARTE DO CÓDIGO DE FORMA DETALHADA, É RECOMENDADO O USO DA EXTENSÃO PARA ENTENDIMENTO DO PROJETO.
+
 #Importando as bibliotecas necessárias
 from flask import Flask, request, jsonify, render_template
 from google import genai
 import json
+import base64
 import os
 from dotenv import load_dotenv
 
@@ -9,31 +13,65 @@ load_dotenv()
 app = Flask(__name__)
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY")) #Substitui a real chave de API por uma variável de ambiente para segurança
 
-#Quando o usuário acessar "/", chama a função abaixo e renderiza index.html
+#*Quando o usuário acessar "/", chama a função abaixo e renderiza index.html
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
 # @app.route("/", methods=["POST"])
-# def teste():
+# def iaRequest():
 #     return jsonify({"resposta": "Conexao funcionando! (console.log)"})
 
-#Quando o usuário enviar um POST para "/", chama a função abaixo, 
+#*Quando o usuário enviar um POST para "/", chama a função abaixo, 
 #que gera uma resposta usando o modelo Gemini e retorna a resposta em formato JSON
 @app.route("/", methods=["POST"])
-def teste():
+def iaRequest():
+
+    #? Processamento da imagem enviada pelo usuário
+    # A variável "fotoDropada" é a imagem que foi enviada pelo usuário, acessada através do nome "arquivo"
+    fotoDropada = request.files["arquivo"] 
+     # A variável "fotoBase64" é a imagem convertida para Base64, para que possa ser enviada para o Gemini
+    fotoBase64 = base64.b64encode(fotoDropada.read()).decode("utf-8")
+    # A variável "fotoTipo" é o tipo da imagem (ex: "image/jpeg"), para que o Gemini saiba como interpretar a imagem
+    fotoTipo = fotoDropada.content_type 
+
+    #? Geração da resposta usando Gemini
     resposta = client.models.generate_content(
         model="gemini-2.5-flash-lite",
-        contents="""Retorne APENAS um JSON válido, sem nenhum texto adicional, sem explicações, sem markdown, sem ```json. 
-        Neste formato exato:
-        {
-            "teste": "Conexao funcionando! (Google Gemini, com JSON)"
-        }
-        """
+        
+        #* O Gemini aceita uma array de objetos, sendo cada objeto uma array! 
+        #neste caso, usaremos apenas uma mensagem, mas ela terá duas partes: a imagem e o texto com as instruções para o Gemini responder apenas com JSON
+        contents=[ 
+            {
+                "parts": [
+                    {
+                        #Seção 1 do prompt, onde Gemini recebe a imagem, com o tipo e os dados em Base64
+                        "inline_data": {
+                            "mime_type": fotoTipo,
+                            "data": fotoBase64
+                        }
+                    },
+                    {
+                        #Seção 2 do prompt, onde damos as instruções para o Gemini responder apenas com JSON, sem nenhum texto adicional
+                        "text": 
+                            """
+                            Leia e entenda a imagem enviada. Na seção "valores do JSON, escreva, com poucas palavras, o que você entendeu da imagem.
+                            Retorne APENAS um JSON válido, sem nenhum texto adicional, sem explicações, sem markdown, sem ```json. 
+                            Neste formato exato (exceto na secão "valores", onde você deve escrever o que entendeu da imagem):
+                            {
+                                "teste": "Conexao funcionando! (Google Gemini, com JSON e reconhecimento de foto)",
+                                "valores": ""
+                            }
+                            """
+                    }
+                ]
+            }
+            
+        ]
     )
 
-    #A resposta do Gemini vem em um formato de texto, então precisamos extrair o JSON dela.
-    texto_resposta = resposta.text
+    #Alteração do retorno da IA para garantir que é retornado apenas como JSON.
+    texto_resposta = resposta.text #* Python retorna apenas a parte "text", não importando a parte da imagem, que é a parte "inline_data"
     texto_resposta = texto_resposta.replace("```json", "").replace("```", "").strip() #* Remove as marcações de código e espaços extras
 
     print("Resposta do Gemini:", texto_resposta) # Imprime a resposta bruta do Gemini para depuração
--- static/js/script.js
+++ static/js/script.js
@@ -9,9 +9,20 @@ document.querySelector("#drop-zone").addEventListener("dragover", async (e) => {
 })
 
 document.querySelector("#drop-zone").addEventListener("drop", async (e) => {
-    e.preventDefault()
-    const resposta = await fetch("/", { method: "POST" }) // Envia um POST para o endpoint "/"
-    const dados = await resposta.json() // a variável "dados" é uma variável auxiliar, que armazena a resposta do backend, convertida para JSON
-    console.log(dados.teste)
+    e.preventDefault() // Previne o comportamento padrão do navegador, que é abrir a imagem em uma nova aba
+
+    const fotoDropada = e.dataTransfer.files[0] // A variável "arquivo" é a primeira imagem que foi dropada no container
+    const formData = new FormData() // A variável "formData" é uma variável auxiliar, que armazena os dados que serão enviados para o backend
+    formData.append("arquivo", fotoDropada) // Adiciona a imagem ao formData, com o nome "arquivo"
+
+    //* fetch na API que envia a imagem e recebe a resposta do backend, que é a leitura da IA da imagem
+    const resposta = await fetch("/", { 
+        method: "POST", 
+        body: formData
+    }) // Envia um POST para o endpoint "/"
+    const retornoIA = await resposta.json() // a variável "dados" é uma variável auxiliar, que armazena a resposta do backend, convertida para JSON
+
+    console.log(retornoIA.teste) //Retorna "Conexao funcionando! (Google Gemini, com JSON e reconhecimento de foto)"
+    console.log(retornoIA.valores) //Retorna a leitura da IA da Imagem
 })
 
